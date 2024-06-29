3ee66a4cd97f761205c7f46f3b4ab3108613b0b3
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2024-06-29 17:45:48 +0100

    added Sass and fonts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass';
 
 export default defineConfig({
   server: {
@@ -11,6 +12,13 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
